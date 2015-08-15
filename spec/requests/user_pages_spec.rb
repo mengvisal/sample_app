05547a1cc1096fbf6da8664a82d279ed9d7334d1
Visal Meng <mengvisal@gmail.com>
2015-08-15 21:00:22 +0700

    Finish signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -32,6 +32,8 @@ describe "User pages" do
     end
 
     describe "with valid information" do
+
+
       before do
         fill_in "Name",         with: "Example User"
         fill_in "Email",        with: "user@example.com"
@@ -42,8 +44,16 @@ describe "User pages" do
       it "should create a user" do
         expect { click_button submit }.to change(User, :count).by(1)
       end
+
+
+      describe "after saving the user" do
+        before { click_button submit }
+        let(:user) { User.find_by(email: 'user@example.com') }
+
+        it { should have_link('Sign out') }
+        it { should have_title(user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+      end
     end
   end
-
-  
 end
